344a382b00d59fea299e05259be83cc1cbe9b36b
vsevaq <vsevolodq@gmail.com>
2022-08-04 13:25:48 +0300

    Create a skeleton of project and added a Ball

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          'Ask Me Anything',
+        ),
+      ),
+     body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              print('I got clicked');
+            });
+          },
+          child: Image(
+            image: AssetImage('images/ball1.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
